125f8b955707950d9b9ba464391b9bed845ba6a7
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-03-06 18:45:26 +0200

    feat: Add bitwise operators to Bitmap class plus examples

--- client/main.js
+++ client/main.js
@@ -1,13 +1,13 @@
-class CanvasBitmap {
+class Bitmap {
   constructor (bitString) {
     const MAX_BITWISE_LENGTH = 32
 
     if (typeof bitString === 'string') {
       this.segments = this._divideBitString(bitString, MAX_BITWISE_LENGTH)
-    } else if (Array.isArray(bitString)) {
+    } else if (Array.isArray(bitString) && typeof bitString === 'string') {
       this.segments = bitString
     } else {
-      throw new Error('Invalid Bitstring')
+      throw new Error('Invalid Bitstring. Type must be Array<String> or String')
     }
   }
 
@@ -29,6 +29,26 @@ class CanvasBitmap {
         i++
     }
     return result
+  }
+
+  // Bitmap operations
+  // We always assume A.length === B.length
+  static OR = (A, B) => Bitmap._bitwise(A, B, ((A, B) => A | B))
+  static AND = (A, B) => Bitmap._bitwise(A, B, ((A, B) => A & B))
+  static XOR = (A, B) => Bitmap._bitwise(A, B, ((A, B) => A ^ B))
+  // Same as before but we use >>> to get a non-negative/unsigned result
+  static NOT = A => new Bitmap(A.segments.map(segment => ((~ parseInt(segment, 2) >>> 0).toString(2))))
+
+  static _bitwise (A, B, op) {
+    return new Bitmap(A.segments.map((segment, i) => {
+      // the strings need to be parsed into integers for bitwise operations
+      return op(parseInt(segment, 2), parseInt(B.segments[i], 2))
+        // then put back as a string for storage
+        .toString(2)
+        // but this trims upto the most significant bit
+        // so we pad it back to it's orginal length
+        .padStart(segment.length, '0')
+    }))
   }
 }
 
@@ -124,8 +144,18 @@ function run () {
   const board = new Board(500, 10, '#canvas')
   board.drawBoard()
 
-  let test = new CanvasBitmap('0'.repeat(70) + '11' + '0'.repeat(28))
-  board.drawBitmap(test, 'black')
+  let ships = new Bitmap('0'.repeat(70) + '11' + '0'.repeat(28))
+
+  let attacks = new Bitmap('0'.repeat(71) + '11' + '0'.repeat(27))
+  board.drawBitmap(ships, 'black')
+
+  // Ships & Attacks
+  let hits = Bitmap.AND(ships, attacks)
+  board.drawBitmap(hits, 'green')
+
+  // ~Ships & Attacks
+  let misses = Bitmap.AND(Bitmap.NOT(ships), attacks)
+  board.drawBitmap(misses, 'lightgray')
 }
 
 window.onload = run
