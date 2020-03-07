a643432fa6ad73901de2fa95d1ed6a4cd87190af
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-03-07 08:21:09 +0200

    refactor: Move game logic into own class

---
+++ client/Game.js
@@ -0,0 +1,66 @@
+import Board from './Board.js'
+import Bitmap from './Bitmap.js'
+import Ship from './Ship.js'
+
+export default class Game {
+  constructor (dimensions, divisions, canvasID) {
+    this.divisions = divisions
+    this.size = divisions ** 2
+
+    this.gameBoard = new Board(dimensions, divisions, canvasID)
+
+    this.SEA_COLOR = 'lightblue'
+    this.seaMap = new Bitmap('1'.repeat(this.size))
+
+    this.shipMap = new Bitmap('0'.repeat(100))
+
+    this.ships = {
+      Carrier: new Ship(5),
+      Battleship: new Ship(4),
+      Destroyer: new Ship(3),
+      Submarine: new Ship(3),
+      Patrol: new Ship(2),
+    }
+
+    this.attackMap = new Bitmap('0'.repeat(100))
+  }
+
+  clearBoard () {
+    this.gameBoard.drawBitmap(this.seaMap, this.SEA_COLOR)
+  }
+
+  getHits () { return Bitmap.AND(this.shipMap, this.attackMap) }
+
+  getMiss () { return Bitmap.AND(Bitmap.NOT(this.shipMap), this.attackMap) }
+
+  placeShip (ship) {
+    const handleShipPlacement = ({ detail: square }) => {
+      // "erase" the ship by drawing ocean over it
+      if (ship.anchor) this.gameBoard.drawBitmap(ship.bounds, this.SEA_COLOR, ship.anchor, { skipCheck: true })
+      ship.anchor = square
+      this.gameBoard.drawBitmap(ship.bounds, 'black', square)
+    }
+
+    this.gameBoard.canvas.addEventListener('boardClicked', handleShipPlacement)
+
+    return ['boardClicked', handleShipPlacement]
+  }
+
+  start () {
+    this.gameBoard.drawBoard()
+    const cleanup = this.placeShip(this.ships.Destroyer)
+    // this.gameBoard.canvas.removeEventListener(...cleanup)
+
+    // ROTATION
+    window.addEventListener('keydown', e => {
+      if (String.fromCharCode(e.keyCode) === 'R') {
+        if (this.ships.Destroyer.anchor) {
+          // "erase" the ship
+          this.gameBoard.drawBitmap(this.ships.Destroyer.bounds, this.SEA_COLOR, this.ships.Destroyer.anchor, { skipCheck: true })
+          this.ships.Destroyer.anchor = null
+        }
+        this.ships.Destroyer.rotate()
+      }
+    })
+  }
+}
--- client/main.js
+++ client/main.js
@@ -1,28 +1,7 @@
-import Board from './Board.js'
-import Square from './Square.js'
-import Bitmap from './Bitmap.js'
-import Ship from './Ship.js'
+import Game from './Game.js'
 
 function run () {
-  const board = new Board(500, 10, '#canvas')
-  board.drawBoard()
-
-  const shipBoard = new Bitmap('0'.repeat(100))
-  const Carrier = new Ship(5)
-
-  const ships = Bitmap.OR(shipBoard, Carrier.bounds)
-
-  board.drawBitmap(ships, 'black')
-
-  const attacks = new Bitmap('0'.repeat(71) + '11' + '0'.repeat(27))
-
-  // Ships & Attacks
-  const hits = Bitmap.AND(ships, attacks)
-  board.drawBitmap(hits, 'green')
-
-  // ~Ships & Attacks
-  const misses = Bitmap.AND(Bitmap.NOT(ships), attacks)
-  board.drawBitmap(misses, 'lightgray')
+  new Game(500, 10, '#canvas').start()
 }
 
 window.onload = run
