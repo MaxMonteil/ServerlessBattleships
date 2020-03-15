2bbb318613f4b2d0a6e444e849b6ea0175c6321b
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-03-15 12:47:03 +0200

    feat: Add logic to put players together in a game

--- server/devServer.py
+++ server/devServer.py
@@ -1,25 +1,70 @@
+from enum import Enum
 from uuid import uuid4
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-players = {}
+lobby = []
+games = {}
 
 
-@app.route("/api/v1/player", methods=["GET"])
-def api_player():
-    playerID = str(uuid4())[:8]
-    players[playerID] = {"shipmap": None}
-    return jsonify(playerID), 200
+class Player(Enum):
+    ONE = 0
+    TWO = 1
+
+
+def new_player():
+    return {
+        "shipmap": None,
+        "attacks": None,
+    }
+
+
+def get_waiting_game():
+    if len(lobby) == 0:
+        return None
+    else:
+        return lobby.pop()
+
+
+@app.route("/api/v1/game", methods=["GET"])
+def api_lobby():
+    gameID = get_waiting_game()
+
+    if gameID is None:
+        gameID = str(uuid4())[:8]
+        lobby.append(gameID)
+        games[gameID] = [new_player()]
+        playerID = Player.ONE
+    else:
+        games[gameID].append(new_player())
+        playerID = Player.TWO
+
+    return jsonify({"game": gameID, "player": playerID.value}), 200
 
 
 @app.route("/api/v1/shipmap", methods=["GET", "POST"])
 def api_shipmap():
+    gameID = request.args["game"]
+    playerID = int(request.args["player"])
+
     if request.method == "GET":
-        return jsonify(players[request.args["player"]]["shipmap"]), 200
+        return jsonify(games[gameID][playerID]["shipmap"]), 200
     else:
-        players[request.args["player"]]["shipmap"] = request.json
+        games[gameID][playerID]["shipmap"] = request.json
+        return jsonify({"endpoint": request.base_url}), 201
+
+
+@app.route("/api/v1/attack", methods=["GET", "POST"])
+def api_attack():
+    gameID = request.args["game"]
+    playerID = int(request.args["player"])
+
+    if request.method == "GET":
+        return jsonify(games[gameID][playerID]["attack"]), 200
+    else:
+        games[gameID][playerID]["attack"] = request.json
         return jsonify({"endpoint": request.base_url}), 201
 
 
