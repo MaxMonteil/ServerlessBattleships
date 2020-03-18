3fcae4850ac467e3c0e9cd490b7ad534c3a3ac65
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-03-18 18:54:47 +0200

    fix: Another bug with rotation and collisions

--- client/classes/ShipsBoard.js
+++ client/classes/ShipsBoard.js
@@ -4,20 +4,22 @@ import Bitmap from './Bitmap.js'
 import Board, { GRID_DIVISIONS } from './Board.js'
 import Ship from './Ship.js'
 
-const CLICK_EVENT = 'ships-board-clicked'
+const CANVAS_CLICK_EVENT = 'ships-board-clicked'
 
 export default class ShipsBoard extends Board {
-  constructor ({ size, id }) {
-    super(size, id.canvas, CLICK_EVENT)
+  constructor ({ size, id, form }) {
+    super(size, id.canvas, CANVAS_CLICK_EVENT)
 
-    // SETUP
+    // REFERENCES
     this.window = window
     this.shipsSection = document.getElementById(id.section)
     this.shipBoardInputs = document.getElementById(id.shipBoardInputs)
     this.orientationDisplay = document.getElementById(id.orientationDisplay)
-    this.shipSelectForm = document.forms[id.shipSelectForm]
-    this.endShipPlacementForm = document.forms[id.endShipPlacementForm]
-    this.placementWait = document.getElementById(id.placementWait)
+
+    // Forms
+    this.shipSelectForm = document.forms[form.shipSelect.id]
+    this.shipSelectFormName = form.shipSelect.name
+    this.endShipPlacementForm = document.forms[form.endShipPlacement.id]
 
     // DATA
     this.api = new ApiService()
@@ -27,9 +29,9 @@ export default class ShipsBoard extends Board {
     this.ships = {
       Carrier: new Ship(5),
       Battleship: new Ship(4),
-      Destroyer: new Ship(3),
-      Submarine: new Ship(3),
-      Patrol: new Ship(2),
+      // Destroyer: new Ship(3),
+      // Submarine: new Ship(3),
+      // Patrol: new Ship(2),
     }
 
     this.selectedShip = null
@@ -39,46 +41,8 @@ export default class ShipsBoard extends Board {
     this.shipsSection.style.visibility = 'visible'
     super.drawBoard()
     this.setupListeners()
-  }
-
-  setupListeners () {
-    this.listeners = {
-      window: {
-        keydown: e => this._handleShipRotation(e),
-      },
-      shipSelectForm: {
-        submit: e => this._handleShipSelection(e),
-      },
-      endShipPlacementForm: {
-        submit: e => this._handlePlacementEnd(e),
-      },
-      canvas: {
-        [CLICK_EVENT]: null,
-      },
-    }
-
-    for (const [target, listener] of Object.entries(this.listeners)) {
-      for (const [event, handler] of Object.entries(listener)) {
-        this[target].addEventListener(event, handler)
-      }
-    }
 
     this.selectShip(this.ships.Carrier)
-  }
-
-  teardownListeners (target = null) {
-    if (target !== null) {
-      for (const [event, handler] of Object.entries(this.listeners[target])) {
-        this[target].removeEventListener(event, handler)
-      }
-      return
-    }
-
-    for (const [target, listener] of Object.entries(this.listeners)) {
-      for (const [event, handler] of Object.entries(listener)) {
-        this[target].removeEventListener(event, handler)
-      }
-    }
   }
 
   selectShip (target, selector = null) {
@@ -100,13 +64,15 @@ export default class ShipsBoard extends Board {
       const { placementValid, map } = this.placeShip(ship, square)
 
       if (placementValid) {
-        this.shipMap.update(map)
+        this.shipMap.update(map.bitString)
         super.drawMap(this.shipMap, { fill: this.selectedShip.color }, true)
-      } else this.squares[square].flash('red')
+      } else {
+        this.squares[square].flash('red')
+      }
     }
 
-    this.canvas.addEventListener(CLICK_EVENT, shipWatcher)
-    this.listeners.canvas[CLICK_EVENT] = shipWatcher
+    this.canvas.addEventListener(CANVAS_CLICK_EVENT, shipWatcher)
+    this.listeners.canvas[CANVAS_CLICK_EVENT] = shipWatcher
   }
 
   placeShip (ship, start) {
@@ -124,23 +90,23 @@ export default class ShipsBoard extends Board {
     // When placing an already present ship, remove it to allow positions that might overlap with itself
     const collisionCheckMap = ship.anchor === null
       ? this.shipMap
-      : new Bitmap(this.moveShip(ship, false))
+      : new Bitmap(this.removeShip(ship, false))
     // Check for collisions against other ships
     const collisions = !Bitmap.EQ(Bitmap.AND(collisionCheckMap, paddedShip), Bitmap.FALSE(this.size))
 
     if (!yValid || !xValid || collisions) return { placementValid: false, map: null }
 
     // this ship is already somewhere on the map
-    if (ship.anchor !== null) this.shipMap.update(this.moveShip(ship))
+    if (ship.anchor !== null) this.shipMap.update(this.removeShip(ship))
 
     ship.anchor = start
     return {
       placementValid: true,
-      map: Bitmap.OR(this.shipMap, paddedShip).bitString,
+      map: Bitmap.OR(this.shipMap, paddedShip),
     }
   }
 
-  moveShip (ship, removeAnchor = true) {
+  removeShip (ship, removeAnchor = true) {
     const padShip = Ship.padBounds(Ship.getOffsetIndices(ship, ship.anchor, GRID_DIVISIONS), this.size)
     if (removeAnchor) ship.anchor = null
     return Bitmap.AND(this.shipMap, Bitmap.NOT(padShip)).bitString
@@ -158,12 +124,50 @@ export default class ShipsBoard extends Board {
     return Object.values(this.ships).every(ship => ship.anchor !== null)
   }
 
+  setupListeners () {
+    this.listeners = {
+      window: {
+        keydown: e => this._handleShipRotation(e),
+      },
+      shipSelectForm: {
+        submit: e => this._handleShipSelection(e),
+      },
+      endShipPlacementForm: {
+        submit: e => this._handlePlacementEnd(e),
+      },
+      canvas: {
+        [CANVAS_CLICK_EVENT]: null,
+      },
+    }
+
+    for (const [target, listener] of Object.entries(this.listeners)) {
+      for (const [event, handler] of Object.entries(listener)) {
+        this[target].addEventListener(event, handler)
+      }
+    }
+  }
+
+  teardownListeners (target = null) {
+    if (target !== null) {
+      for (const [event, handler] of Object.entries(this.listeners[target])) {
+        this[target].removeEventListener(event, handler)
+      }
+      return
+    }
+
+    for (const [target, listener] of Object.entries(this.listeners)) {
+      for (const [event, handler] of Object.entries(listener)) {
+        this[target].removeEventListener(event, handler)
+      }
+    }
+  }
+
   _handleShipRotation (e) {
     if (String.fromCharCode(e.keyCode) !== 'R') return
 
     // The ship is present on the board somewhere, remove it before rotating
     if (this.selectedShip.anchor !== null) {
-      this.shipMap.update(this.moveShip(this.selectedShip, false))
+      this.shipMap.update(this.removeShip(this.selectedShip))
       super.drawMap(this.shipMap, { fill: this.selectedShip.color }, true)
     }
 
@@ -173,7 +177,7 @@ export default class ShipsBoard extends Board {
 
   _handleShipSelection (e) {
     e.preventDefault()
-    this.selectShip(e.target, 'ship_select')
+    this.selectShip(e.target, this.shipSelectFormName)
     this.updateOrientation()
   }
 
